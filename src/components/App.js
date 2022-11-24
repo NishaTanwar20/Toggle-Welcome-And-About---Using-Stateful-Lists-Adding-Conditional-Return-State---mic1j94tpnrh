41037bf0dd06c92f4269931b2a1656b7f4d098e3
Nisha Tanwar <nishatanwar2706@gmail.com>
2022-11-24 17:17:32 +0530

    commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React , {useState} from "react";
 import "../styles/App.css";
 
 const App = () => {
+  const [text , setText] = useState(true);
+  const handleClick = () => {
+    setText(!text)
+  }
   return (
     <div id="main">
+      {text ? 
       <div id="welcome-div">
         <h1>Welcome to Newton School!!</h1>
-      </div>
+      </div> :
       <div id="about-div">
         <h1>
           Newton School is a neo-university providing highly immersive tech
           learning to millions of students, to tap into new-age tech
           opportunities.
         </h1>
-      </div>
-      <button id="toggle">Toggle</button>
+      </div>}
+      <button id="toggle" onClick = {()=>handleClick()}>Toggle</button>
     </div>
   );
 };
